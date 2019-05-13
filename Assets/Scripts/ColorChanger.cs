a31abcdb013fe9ecb786c02854e443d6ecf4df0a
fariazz <fariazz@gmail.com>
2019-05-13 21:01:59 +1000

    first commit

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public void ChangeToRed()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void ChangeToYellow()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void ChangeToGreen()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# VR Controller for the Masses
+
+Source code from my presentation at Unite Sydney 2019.
+
+Feel free to use this code in your own projects!
+
+To learn more about VR development make sure to check out our [Virtual Reality Mini-Degree](https://academy.zenva.com/product/the-complete-virtual-reality-game-development-course/?zva_src=github-vr).
